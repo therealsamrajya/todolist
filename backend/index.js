9887be342495713bdce5b2a1920a98528bccec44
therealsamrajya <samrajyadai@gmail.com>
2024-09-15 09:04:01 +0545

    nv

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://todolist-91b6.onrender.com"],
     credentials: true,
   })
 );
